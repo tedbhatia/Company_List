3925af6a4d9bea1c5c6223487cbe5f3e8a466382
tedbhatia <ted.bhatia@gmail.com>
2017-03-21 00:03:08 +0530

    fifth

--- app/src/main/java/dungeon/dr/company_list/CustomAdapter.java
+++ app/src/main/java/dungeon/dr/company_list/CustomAdapter.java
@@ -10,9 +10,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
-/**
- * Created by lenovo on 14-Dec-16.
- */
+//Custom Adapter
 public class CustomAdapter extends ArrayAdapter {
     public CustomAdapter(Context context, List<String> test) {
         super(context,R.layout.custom_row, test);
@@ -24,11 +22,11 @@ public class CustomAdapter extends ArrayAdapter {
         LayoutInflater ayu_layoutInflater = LayoutInflater.from(getContext());
         View custom_view = ayu_layoutInflater.inflate(R.layout.custom_row, parent, false);
 
+	//Sets text and images to the references on the XML
         String single_text = (String) getItem(position);
         TextView ayu_text = (TextView) custom_view.findViewById(R.id.text);
         ImageView ayu_image = (ImageView) custom_view.findViewById(R.id.image);
 
-        /*ayu_image.setImageResource(image url);*/
         ayu_text.setText(single_text);
 
         return custom_view;
